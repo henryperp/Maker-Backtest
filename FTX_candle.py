ba0636b75b86b6ae083de4e7ed600b14588c3005
Henry <henry@perp.fi>
2022-02-06 22:34:36 +0800

    add time setting and custom time

--- FTX_candle.py
+++ FTX_candle.py
@@ -8,39 +8,44 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config/config.ini')
 
-Market = config['Setting']['Market']
-Start = int(datetime.strptime(config['Setting']['Starttime'], "%Y-%m-%d %H:%M").timestamp())
+Market = config['Setting']['Market'] + '-PERP'
+Time = int(config['Setting']['Time'])
+Start = int(datetime.strptime(config['Setting']['Starttime'], "%Y-%m-%d %H:%M:%S").timestamp())
 End = config['Setting']['Endtime']
 if End == 'now' :
     End = int(datetime.now().timestamp())
 else :
-    End = int(datetime.strptime(End, "%Y-%m-%d %H:%M").timestamp())
+    End = int(datetime.strptime(End, "%Y-%m-%d %H:%M:%S").timestamp())
 
 
 x = 0
 Start2 = Start
-file_name = 'candle_' + Market + '.csv'
+file_name = 'candle_' + Market + '_Time_' +  str(Time) + '.csv'
 path = 'candle/' +file_name
 
 with open(path, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['Date', 'Open', 'High', 'Low', 'Close'])
 
-while x < 999 :
-    Time0 = Start + x * (3600000) + 3600
-    Time1 = Start + (x + 1) * (3600000)
+while x < 999999 :
+    Time0 = Start + x * (Time*1000) + Time
+    Time1 = Start + (x + 1) * (Time*1000)
     if Time1 >= End :
         Time1 = End
-        x = 1000
+        x = 1000000
 
     print(f'Time0: {Time0}  Time1: {Time1}, End {End}')
-    URL = 'https://ftx.com/api/indexes/'+  Market + '/candles?resolution=3600&start_time=' + str(Time0) + '&end_time=' + str(Time1)
+    URL = 'https://ftx.com/api/markets/'+  Market  + '/candles?resolution=' + str(Time) + '&start_time=' + str(Time0) + '&end_time=' + str(Time1)
     resp_F = (requests.get(URL)).json()
     
-    for i in range(len(resp_F['result'])):
-        with open(path, 'a', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow([resp_F['result'][i]['startTime'][0:10:]+" "+resp_F['result'][i]['startTime'][11:19:], str(resp_F['result'][i]['open']), str(resp_F['result'][i]['high']), str(resp_F['result'][i]['low']), str(resp_F['result'][i]['close'])])
+    try :
+        for i in range(len(resp_F['result'])):
+            with open(path, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow([resp_F['result'][i]['startTime'][0:10:]+" "+resp_F['result'][i]['startTime'][11:19:], str(resp_F['result'][i]['open']), str(resp_F['result'][i]['high']), str(resp_F['result'][i]['low']), str(resp_F['result'][i]['close'])])
+    except :
+        print('Unsupported candle resolution, time : 1, 60, 300, 3600')
+        x = 1000000
 
     x = x +1
 
--- Full_range_maker.py
+++ Full_range_maker.py
@@ -8,26 +8,55 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config/config.ini')
 
-Market = config['Full_range_setting']['Market']
+Market = config['Full_range_setting']['Market'] + '-PERP'
+Time = config['Full_range_setting']['Time']
 Liquidity = float(config['Full_range_setting']['Liquidity'])
+Starttime = config['Full_range_setting']['Starttime']
+Starttime_S = int(datetime.strptime(Starttime, "%Y-%m-%d %H:%M:%S").timestamp())
+Endtime = config['Full_range_setting']['Endtime']
+if Endtime == 'now' :
+    Endtime = str(datetime.strptime(Starttime, "%Y-%m-%d %H:%M:%S"))
+    Endtime_S = int(datetime.now().timestamp())
+else :
+    Endtime_S = int(datetime.strptime(Endtime, "%Y-%m-%d %H:%M:%S").timestamp())
 Trigger = 1000000
 
-file_name = 'candle_' + Market + '.csv'
-path = 'candle/' +file_name
 
+#讀取K線資料
+file_name = 'candle_' + Market + '_Time_' +  str(Time) + '.csv'
+path = 'candle/' + file_name
 f = open(path, 'r')
 crv_price = list(reader(f, delimiter=','))
 
-#輸出設定
-output_path_file = 'Full_range_maker_' + Market + '.csv'
+#輸出檔案設定
+output_path_file = 'Full_range_maker_' + Market + '_Time_' +  str(Time) + '.csv'
 output_path ='output/' +output_path_file
 
 with open(output_path, 'w', newline='') as outputfile:
     writer = csv.writer(outputfile)
     writer.writerow(['Date', 'Open', 'High', 'Low', 'Close', 'Mid_price', 'r', 'Impermanent_loss'])
 
+#回測時間過濾
+Data_Start = crv_price[1][0]
+Data_Start_S = int(datetime.strptime(Data_Start, "%Y-%m-%d %H:%M:%S").timestamp())
+time_priod = int(datetime.strptime(crv_price[2][0], "%Y-%m-%d %H:%M:%S").timestamp()) -int(datetime.strptime(Data_Start, "%Y-%m-%d %H:%M:%S").timestamp())
+#過濾開始時間
+if Data_Start_S <= Starttime_S :
+    start_count = int((Starttime_S - Data_Start_S) / time_priod) +1
+else :
+    start_count = 1
+
+#過濾結束時間
+Data_End = crv_price[len(crv_price)-1][0]
+Data_End_S = int(datetime.strptime(Data_End, "%Y-%m-%d %H:%M:%S").timestamp())
+if Data_End_S >= Endtime_S :
+    end_count = int((Endtime_S - Data_Start_S) / time_priod) +1
+else :
+    end_count = len(crv_price)-1
+
+
 #main Strategy
-for i in range(1,len(crv_price)):
+for i in range(start_count,end_count):
     Date = crv_price[i][0]
     Open_price = float(crv_price[i][1])
     High_price = float(crv_price[i][2])
@@ -35,7 +64,7 @@ for i in range(1,len(crv_price)):
     Close_price = float(crv_price[i][4])
 
     #計算中間價
-    if i > 1 :
+    if i > start_count :
         #判斷前一小時是否觸及Trigger
         if float(crv_price[i-1][3]) <= (crv_price[i-1][5] / Trigger) :
             Mid_price = Mid_price[i -1] / Trigger
